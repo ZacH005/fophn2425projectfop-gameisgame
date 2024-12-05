9fc2280f738501f8f0f596686c6656a5df6e5f5b
ZH <zachary.hooser@gmail.com>
2024-12-05 18:04:58 +0100

    Added player class, alongside added boundary for the map, camera follows player, player collides with boundary

--- core/src/de/tum/cit/fop/maze/GameScreen.java
+++ core/src/de/tum/cit/fop/maze/GameScreen.java
@@ -96,8 +96,6 @@ public class GameScreen implements Screen {
         map.render();
         player.render(game.getSpriteBatch());
 
-
-
         // Draw the character next to the text :) / We can reuse sinusInput here
 //        game.getSpriteBatch().draw(currentAnimation.getKeyFrame(sinusInput, true), camera.position.x-16, camera.position.y -32, 16, 32);
         game.getSpriteBatch().end(); // Important to call this after drawing everything
