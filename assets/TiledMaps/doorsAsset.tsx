9c265e109a6f79189cb4d39c2612f5da3c8b0bba
ZH <zachary.hooser@gmail.com>
2025-01-09 14:09:53 +0100

    All random files

---
+++ assets/TiledMaps/doorsAsset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="doorsAsset" tilewidth="16" tileheight="16" tilecount="96" columns="12">
+ <image source="tilesets/adventure/free_doors2.png" width="192" height="128"/>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" template="doorTemplate2.tx" x="0" y="0"/>
+  </objectgroup>
+ </tile>
+</tileset>
